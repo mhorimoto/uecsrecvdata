56ef30c35a40cb6a9c465e7f2ce4deabac7bc378
Masafumi Horimoto on uecsgw01 <horimoto@holly-linux.com>
2024-09-11 01:20:55 +0900

    MQTT interface implementation testing supported version

--- recvdata.py
+++ recvdata.py
@@ -8,11 +8,16 @@ import datetime
 import time
 import syslog
 import xml.etree.ElementTree as ET
+import paho.mqtt.client as mqtt
+import configparser
 
-VERSION="2.30"
+config = configparser.ConfigParser()
+config.read('/usr/local/etc/uecsgw/config.ini')
+
+VERSION="3.00"
 HOST = ''
-PORT = 16520
-LOGF = "/var/log/uecs/recvdata.log"
+PORT = int(config['uecs']['Port'])
+print(PORT)
 TMPD = "/tmp/ckua-"
 
 s = socket(AF_INET,SOCK_DGRAM)
@@ -22,29 +27,39 @@ d = "{0:4d}/{1:02d}/{2:02d}".format(a.year,a.month,a.day)
 t = "{0:02d}:{1:02d}:{2:02d}".format(a.hour,a.minute,a.second)
 x = "{0}-{1}".format(d,t)
 syslog.syslog(syslog.LOG_INFO,"{0} START UECS recvdata.py VER.{1}".format(x,VERSION))
+hn     = config['mqtt']['Id']
+client = mqtt.Client(hn)
+client.username_pw_set(config['mqtt']['User'],config['mqtt']['Passwd'])
+client.connect(config['mqtt']['BrokerHost'],int(config['mqtt']['Port']), 60) 
+client.loop_start()
 
 while True:
   msg, address = s.recvfrom(4096)
-  a=datetime.datetime.now()
-  d="{0:4d}/{1:02d}/{2:02d}".format(a.year,a.month,a.day)
-  t="{0:02d}:{1:02d}:{2:02d}".format(a.hour,a.minute,a.second)
-  x="{0}-{1}".format(d,t)
-  txt = msg.decode()
-  xmlroot = ET.fromstring(msg)
-  xdata = xmlroot.find('DATA')
-  value = xdata.text
-  ccmtype = xdata.get('type')
-  room = xdata.get('room')
-  region = xdata.get('region')
-  order = xdata.get('order')
+  a = datetime.datetime.now()
+  d = "{0:4d}/{1:02d}/{2:02d}".format(a.year,a.month,a.day)
+  t = "{0:02d}:{1:02d}:{2:02d}".format(a.hour,a.minute,a.second)
+  x = "{0}-{1}".format(d,t)
+  txt      = msg.decode()
+  xmlroot  = ET.fromstring(msg)
+  xdata    = xmlroot.find('DATA')
+  value    = xdata.text
+  ccmtype  = xdata.get('type')
+  room     = xdata.get('room')
+  region   = xdata.get('region')
+  order    = xdata.get('order')
   priority = xdata.get('priority')
-  ipa = xmlroot.find('IP').text
-  logm = "{0},{1},{2},{3},{4},{5},{6},{7}".format(x,ccmtype,room,region,order,priority,value,ipa)
+  ipa      = xmlroot.find('IP').text
+  logm     = "{0},{1},{2},{3},{4},{5},{6},{7}".format(x,ccmtype,room,region,order,priority,value,ipa)
   syslog.syslog(syslog.LOG_INFO,logm)
-
-  SMPF=TMPD+ipa+".chk"
+#  if (ccmtype=='WRadiation'):
+  topictop = config['mqtt']['TopicTop']
+  pubtopic = "{0}/data/{1}/{2}/{3}/{4}/{5}".format(topictop,room,region,order,ccmtype,ipa)
+  client.publish(pubtopic,value)
+# endif
+  SMPF = TMPD+ipa+".chk"
   if (os.path.exists(SMPF)):
     os.remove(SMPF)
 
 s.close()
 sys.quit()
+
